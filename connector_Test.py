a19e21c87a44cda04b8fb601b43c0031a3f7c655
Alejandro-Dom <alejandrodomra@gmail.com>
2022-10-04 13:19:38 -0500

    Update connector_Test.py

--- connector_Test.py
+++ connector_Test.py
@@ -1,4 +1,10 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='Alex', password='4892', host = '192.168.1.109', database = 'codigoIoT')
+print("Conectandose a la base de datos")
+cnx = mysql.connector.connect(user='alex', password='4892', host = '192.168.1.109', database = 'codigoIoT')
+print("Conexión realizada")
+print(cnx)
+
+print("Cerrar conexión")
 cnx.close()
+print("Conexión cerrada")
