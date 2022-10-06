29920439f2033ac774a6dfb73e1c615ff380df5f
Alejandro-Dom <alejandrodomra@gmail.com>
2022-10-06 12:16:40 -0500

    Correción del programa query

--- query_mysql.py
+++ query_mysql.py
@@ -1,16 +1,14 @@
 #Bibliotecas
-import datetime
 import mysql.connector
 
 #Conexión
 cnx = mysql.connector.connect(user='alex', password='4892', host = '127.0.0.1', database = 'detector_sintomas')
 cursor = cnx.cursor()
 
-query = ("SELECT id, nombre, protodiagnostico FROM registro WHERE id= 13")
-res = cnx.cmd_query(query)
+query = ("SELECT id, nombre, protodiagnostico FROM registro WHERE id= 13;")
+cursor.execute(query)
 
-print("Respuesta")
-print(res)
+res = cursor.fetchall()
 
-#cursor.close()
-cnx.close()
+for x in res:
+    print (x)
