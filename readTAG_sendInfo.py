8bb8e0c2679d9f2e8b38bfb04daa290c89bf6e60
Alejandro-Dom <alejandrodomra@gmail.com>
2022-10-11 13:16:24 -0500

    Create readTAG_sendInfo.py

---
+++ readTAG_sendInfo.py
@@ -0,0 +1,31 @@
+#Bibliotecas
+import mysql.connector
+import RPi.GPIO as GPIO
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+
+#Iniciar el sensor
+reader = SimpleMFRC522()
+#Conexión
+cnx = mysql.connector.connect(user='alex', password='4892', host = '192.168.1.109', database = 'codigoIoT')
+cursor = cnx.cursor()
+
+#Cuerpo del programa
+try:
+    #Leer el tag
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        insertquery = ("INSERT INTO rfid (nombre,texto,rfid) VALUES (" + text + ",'Test Python 3'," + id +");")
+        cursor.execute(insertquery)
+        cnx.commit()
+        print("La operación del Query fue exitosa")
+        #Cerrar
+        cursor.close()
+        cnx.close()
+        sleep(10)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
