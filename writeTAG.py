833022b3fd1f3cb0cedc87e4b5c4544ded1b1c24
Alejandro-Dom <alejandrodomra@gmail.com>
2022-10-20 14:13:11 -0500

    Update writeTAG.py

--- writeTAG.py
+++ writeTAG.py
@@ -5,9 +5,14 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        text = input('New data:')
-        print("Now place your tag to write")
+        nombre = input('Nombre:')
+        edad = input('Edad:')
+        telefono = input('Telefono:')
+        naci = input('Fecha de nacimiento:')
+        matricula = input('Matrícula:')
+        text = (nombre + "," + edad + "," + telefono + "," + naci + "," + matricula)
+        print("Acerca el TAG al sensor")
         reader.write(text)
-        print("Written")
+        print("Tag escrito")
 finally:
         GPIO.cleanup()
